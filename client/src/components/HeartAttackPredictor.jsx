6512fa502ef0de4231e10d67badfc0ddcbf8dae1
Pragallapati Saketh <pragallapati.saketh@gmail.com>
2025-07-12 15:08:48 +0530

    Update HeartAttackPredictor.jsx

--- client/src/components/HeartAttackPredictor.jsx
+++ client/src/components/HeartAttackPredictor.jsx
@@ -58,7 +58,7 @@ const HeartAttackPredictor = () => {
         Troponin: parseFloat(formData.Troponin)
       };
 
-      const response = await fetch('http://localhost:5000/predict', {
+      const response = await fetch('https://heart-attack-predictor-ozvc.onrender.com/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
